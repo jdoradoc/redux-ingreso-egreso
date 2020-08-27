928c5e10b24b49b1df5138b1f117f720cbc57ed1
Juan Dorado Cebrian <juan.dorado@deimos-space.com>
2020-08-27 16:01:32 +0200

    fixed enviroment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBxK6eNwHoAKYBGDW3cUlhLaGytNIOiWcs',
-    authDomain: 'ingreso-egreso-app.firebaseapp.com',
-    databaseURL: 'https://ingreso-egreso-app.firebaseio.com',
-    projectId: 'ingreso-egreso-app',
-    storageBucket: 'ingreso-egreso-app.appspot.com',
-    messagingSenderId: '577324738477',
-    appId: '1:577324738477:web:e35aca0665073e12d02324'
+    apiKey: 'AIzaSyDNBQAnx49nNxOXStVTXQ9FW7RbyhzF6LY',
+    authDomain: 'ingresoegreso-f8bab.firebaseapp.com',
+    databaseURL: 'https://ingresoegreso-f8bab.firebaseio.com',
+    projectId: 'ingresoegreso-f8bab',
+    storageBucket: 'ingresoegreso-f8bab.appspot.com',
+    messagingSenderId: '259468957458',
+    appId: '1:259468957458:web:5aacf34213377f37a485cb'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDNBQAnx49nNxOXStVTXQ9FW7RbyhzF6LY",
-    authDomain: "ingresoegreso-f8bab.firebaseapp.com",
-    databaseURL: "https://ingresoegreso-f8bab.firebaseio.com",
-    projectId: "ingresoegreso-f8bab",
-    storageBucket: "ingresoegreso-f8bab.appspot.com",
-    messagingSenderId: "259468957458",
-    appId: "1:259468957458:web:5aacf34213377f37a485cb"
+    apiKey: 'AIzaSyDNBQAnx49nNxOXStVTXQ9FW7RbyhzF6LY',
+    authDomain: 'ingresoegreso-f8bab.firebaseapp.com',
+    databaseURL: 'https://ingresoegreso-f8bab.firebaseio.com',
+    projectId: 'ingresoegreso-f8bab',
+    storageBucket: 'ingresoegreso-f8bab.appspot.com',
+    messagingSenderId: '259468957458',
+    appId: '1:259468957458:web:5aacf34213377f37a485cb'
   }
 };
 
